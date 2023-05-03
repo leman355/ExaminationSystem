d4524fc06dcc32c9519c2a27949aca3a0866dc18
leman355 <leman355@gmail.com>
2023-05-03 15:40:24 +0400

    question edit status

--- Web/Areas/Dashboard/Controllers/QuestionController.cs
+++ Web/Areas/Dashboard/Controllers/QuestionController.cs
@@ -50,11 +50,6 @@ namespace Web.Areas.Dashboard.Controllers
                 question.IsDeleted = IsDeleted;
                 await _context.Questions.AddAsync(question);
                 await _context.SaveChangesAsync();
-                //if (Option.Length != Status.Length)
-                //{
-                //    Debug.WriteLine($"Option length: {Option.Length}; Status length: {Status.Length}");
-                //    return BadRequest("Invalid input");
-                //}
                 for (int i = 0; i < Option.Length; i++)
                 {
                     if (Option[i] != null)
@@ -63,9 +58,6 @@ namespace Web.Areas.Dashboard.Controllers
                         Answer answer = new()
                         {
                             Option = Option[i],
-                            //Status = Status[i],
-                            //Status = Status[i] || Request.Form["Status"][i] == "true",
-                            //Status = Status.Length > i ? Status[i] : false ,
                             Status = status,
                         };
                         await _context.Answers.AddAsync(answer);
@@ -88,47 +80,14 @@ namespace Web.Areas.Dashboard.Controllers
             }
         }
 
-
-
-        //public async Task<IActionResult> Create(Question question, List<string> ansIds)
-        //{
-        //    try
-        //    {
-
-        //        //for (int i = 0; i < ansIds.Count; i++)
-        //        //{
-        //        //    Question questionAnswer = new()
-        //        //    {
-        //        //        Answers = ansIds[i]
-        //        //    };
-        //        //    await _context.Questions.AddAsync(questionAnswer);
-        //        //    await _context.SaveChangesAsync();
-        //        //}
-        //        Question questionAnswer = new()
-        //        {
-        //            //List<string> Answers = ansIds[i]
-        //            Id=question.Id,
-        //            Content=question.Content,
-        //        };
-        //        await _context.Questions.AddAsync(questionAnswer);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch (Exception)
-        //    {
-        //        return NotFound();
-        //    }
-        //}
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
             try
             {
-
                 var question = await _context.Questions.FirstOrDefaultAsync(x => x.Id == id);
                 var answers = _context.Answers.ToList();
                 var questionAnswers = _context.QuestionAnswers.Where(x => x.QuestionId == question.Id).ToList();
-
 
                 QuestionEditVM editVM = new()
                 {
@@ -136,64 +95,61 @@ namespace Web.Areas.Dashboard.Controllers
                     Answers = answers,
                     QuestionAnswers = questionAnswers,
                 };
+
                 return View(editVM);
-
             }
-            catch (Exception e)
+            catch (Exception)
             {
-
                 return View();
             }
         }
-        [HttpPost]
-        public async Task<IActionResult> Edit(Question question, int[] ansIds)
-        {
-            //student.UpdatedDate = DateTime.Now;
-            //_context.Students.Update(student);
-            //var studentGroup = _context.StudentGroups.Where(x => x.StudentId == student.Id).ToList();
-            //_context.StudentGroups.RemoveRange(studentGroup);
-            //_context.SaveChanges();
-            //var gr = _context.Groups.FirstOrDefault(x => x.Id == groupId);
-            //StudentGroup stgr = new()
-            //{
-            //    StudentId = student.Id,
-            //    GroupId = gr.Id,
-            //};
-            //_context.StudentGroups.Add(stgr);
-            //_context.SaveChanges();
-            //return RedirectToAction("Index");
 
+        [HttpPost]
+        public async Task<IActionResult> Edit(Question question, string[] Option, bool IsDeleted)
+        {
             try
             {
+                question.IsDeleted = IsDeleted;
                 _context.Questions.Update(question);
+                await _context.SaveChangesAsync();
+                
                 var questionAnswers = _context.QuestionAnswers.Where(x => x.QuestionId == question.Id).ToList();
                 _context.QuestionAnswers.RemoveRange(questionAnswers);
                 await _context.SaveChangesAsync();
 
-                for (int i = 0; i < ansIds.Length; i++)
+            
+                for (int i = 0; i < Option.Length; i++)
                 {
-                    QuestionAnswer questionAnswer = new()
+                    if (Option[i] != null)
                     {
-                        QuestionId = question.Id,
-                        AnswerId = ansIds[i]
-                    };
-                    await _context.QuestionAnswers.AddAsync(questionAnswer);
-                    await _context.SaveChangesAsync();
+                        bool.TryParse(Request.Form[$"Status_{i}"], out bool status);
+                        Answer answer = new()
+                        {
+                            Option = Option[i],
+                            Status = status,
+                        };
+                        await _context.Answers.AddAsync(answer);
+                        await _context.SaveChangesAsync();
+
+                        QuestionAnswer questionAnswer = new()
+                        {
+                            QuestionId = question.Id,
+                            AnswerId = answer.Id
+                        };
+                        await _context.QuestionAnswers.AddAsync(questionAnswer);
+                        await _context.SaveChangesAsync();
+                    }
                 }
-                //Question questionAnswer = new()
-                //{
-                //    //List<string> Answers = ansIds[i]
-                //    Answers = ansIds
-                //};
-                //await _context.Questions.AddAsync(questionAnswer);
-                //await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+
+
+                return RedirectToAction(nameof(Index));
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                return View();
+                return NotFound();
             }
         }
+
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
